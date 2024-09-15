6560ebfc60fdb35cd48d59efee977ed2da343f4e
AnaC <anaclaraderario@gmail.com>
2024-09-14 22:18:03 -0300

    Pagina de agregar libros

--- app/src/main/java/com/example/libros/ui/activity/AddBookActivity.kt
+++ app/src/main/java/com/example/libros/ui/activity/AddBookActivity.kt
@@ -1,7 +1,9 @@
 package com.example.libros.ui.activity
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,5 +15,20 @@ class AddBookActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_add_book)
+        ratingSpinner()
+    }
+
+    //Agregar estados al spinner
+    private fun ratingSpinner(){
+        val ratingSpinner = findViewById<Spinner>(R.id.spinnerState)
+
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.state_array,
+            android.R.layout.simple_spinner_item
+        )
+        // Siempre aparece ese item
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        ratingSpinner.adapter = adapter
     }
 }
